38e98a0615950ef0ea16e052f28ee8672e2dc0dd
cyberlame <cyberlame@no.mail>
2015-07-21 08:47:03 +0300

    ia32

--- build/platform-msvc.mk
+++ build/platform-msvc.mk
@@ -1,6 +1,6 @@
 include $(SRC_PATH)build/msvc-common.mk
 LDFLAGS += user32.lib
-CFLAGS_OPT += -MT
+CFLAGS_OPT += -MT -arch:IA32
 CFLAGS_DEBUG += -MTd -Gm
 
 $(LIBPREFIX)$(PROJECT_NAME).$(SHAREDLIBSUFFIXVER): openh264.res
